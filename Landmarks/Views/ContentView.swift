bbdf5755b85cbd52617bde30b81774338998ee0e
Georgia <georgia.rust@gmail.com>
2022-01-27 11:00:29 -0700

    add favoriting logic

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Georgia Rust on 1/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarksList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
