a83c5b173c983cf87826a252672af8a77998ef1e
Georgia <georgia.rust@gmail.com>
2022-01-27 13:11:42 -0700

    Added favorites toggle and stars

---
+++ Landmarks/Views/FavoriteButton.swift .swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteButton.swift .swift
+//  Landmarks
+//
+//  Created by Georgia Rust on 1/27/22.
+//
+
+import SwiftUI
+
+struct FavoriteButton_swift_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteButton_swift__Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton_swift_()
+    }
+}
--- Landmarks/Views/Landmarks/LandmarksList.swift
+++ Landmarks/Views/Landmarks/LandmarksList.swift
@@ -8,31 +8,39 @@
 import SwiftUI
 
 struct LandmarksList: View{
-    @State private var showFavoritesOnly = true
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
     
     var filteredLandmarks : [Landmark] {
-        landmarks.filter { landmark in
+        modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
     var body: some View {
         NavigationView {
-            List(filteredLandmarks) { landmark in
+            List{
+                Toggle(isOn: $showFavoritesOnly){
+                    Text("Favorites Only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
                 LandmarkRow(landmark: landmark)
+                    }
                 }
+            }
             .navigationTitle("Landmarks")
             }
         }
     }
-}
     
 
 struct LandmarksList_Previews: PreviewProvider {
     static var previews: some View {
         LandmarksList()
+            .environmentObject(ModelData())
                     }
                 }
 
